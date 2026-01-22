e5fe12122b405dd02ab86f8a0a2e5460217c239b
Eda67 <clwr0502@leeds.ac.uk>
2026-01-22 21:22:20 +0000

    add UART in server code

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 from microbit import *
 import radio
 radio.config(channel=42, group=7, length=64)
+radio.on()
 
-display.show('S')
+uart.init(baudrate=115200)
 
 while True:
-  msg= radio.receive()
-  if msg:
-    display.show('R')
-    print(msg)
-    sleep(1000)
-    display.show('S')
+msg= radio. receive()
+if msg:
+uart.write(msg + "\n")
+display.scroll(msg)
+sleep
