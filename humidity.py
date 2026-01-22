cf14b86d8d6be5ad948cf90d0dc197dd699c9b1c
Eda67 <clwr0502@leeds.ac.uk>
2026-01-22 21:41:57 +0000

    Added radio sending in humidity sensor

--- humidity.py
+++ humidity.py
@@ -1,10 +1,16 @@
 from bme688 import *
 from microbit import *
+import radio
+radio.config(channel=42, group=7, length=64)
+radio.on()
 
 init_sensor()
 
 while True:
     read_data_registers()
     humidity = calc_humidity()
-    print("Humidity:", humidity, "%")
+
+    msg = "H," + str(humidity)
+    radio.send(msg)
+
     sleep(500)
