65242e46e60e1d5577da4d0edc41b9298da56c2e
BenWarnock <bvqw2790@leeds.ac.uk>
2026-01-22 12:18:35 +0000

    Add files via upload

---
+++ temperature.py
@@ -0,0 +1,35 @@
+from microbit import *
+import radio
+from bme688 import *
+import time
+
+init_sensor()
+radio.on()
+radio.config(channel =, group =)
+loop =True
+numOfReadings = 0
+
+display.show(Image.HAPPY)
+
+while loop:
+    
+    read_data_registers()
+    temperature = calc_temperature() # degrees C float
+    pressure = calc_pressure() # Pressure Pa int
+    
+    display.scroll("Temperature: {} C".format(temperature))
+    display.scroll("Pressure: {} Pa".format(pressure))
+    
+    temp = ("Temperature: {} C".format(temperature))
+    pres = ("Pressure: {} Pa".format(pressure))
+    
+    radio.send(temp)
+    radio.send(pres)
+    
+    numOfReadings += 1
+    
+    if button_a.is_pressed() or (numOfReadings > 50):
+        display.show(Image.DIAMOND)
+        loop = False
+
+    sleep(5000)
