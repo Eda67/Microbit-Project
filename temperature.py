2f5cc23cd6ca76306c751b23333e438debf42fc1
Syeda <clwr0502@leeds.ac.uk>
2026-01-24 18:04:41 +0000

    Removed pressure and display.scroll

--- temperature.py
+++ temperature.py
@@ -9,22 +9,14 @@ radio.config(channel = 42, group =7)
 loop =True
 numOfReadings = 0
 
-display.show(Image.HAPPY)
-
 while loop:
     
     read_data_registers()
     temperature = calc_temperature() # degrees C float
-    pressure = calc_pressure() # Pressure Pa int
-    
-    display.scroll("Temperature: {} C".format(temperature))
-    display.scroll("Pressure: {} Pa".format(pressure))
     
     temp = ("Temperature: {} C".format(temperature))
-    pres = ("Pressure: {} Pa".format(pressure))
     
     radio.send(temp)
-    radio.send(pres)
     
     numOfReadings += 1
     
@@ -33,4 +25,4 @@ while loop:
         loop = False
 
 
-    sleep(5000)
+    sleep(500)
